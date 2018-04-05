218aec647d9f202693c422b4367c97c4ca04a8fc
ADM_Jarle Pedersen <adm_jarlped@win.ntnu.no>
2018-04-05 10:44:50 +0200

    drop schema before recreating

--- sql/bigbadabom.sql
+++ sql/bigbadabom.sql
@@ -16,6 +16,9 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
+
+DROP SCHEMA IF EXISTS data CASCADE;
+
 --
 -- TOC entry 6 (class 2615 OID 129582)
 -- Name: data; Type: SCHEMA; Schema: -; Owner: postgres
