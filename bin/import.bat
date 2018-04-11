8e6b795cfc9282095a2f977e5d78ccd25d2c6e9b
jarped <mybigb@gmail.com>
2018-04-11 17:21:58 +0200

    update import-script with new paths

--- bin/import.bat
+++ bin/import.bat
@@ -2,7 +2,11 @@ python pgSqlExecutor.py ..\sql\drop.sql ..\config\postgis.json
 
 python pgSqlExecutor.py ..\sql\schema.sql ..\config\postgis.json
 
-for %%f in (..\fme\in\*.fmw) do fme.exe %%f
+cd ..\fme\in
+
+for %%f in (*.fmw) do fme.exe %%f
+
+cd ..\..\bin
 
 python pgSqlExecutor.py ..\sql\post-process.sql ..\config\postgis.json
 
