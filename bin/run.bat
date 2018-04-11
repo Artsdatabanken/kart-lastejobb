b7a82a3ddf22544d77ace9bf6bc14224cbf4a6c3
jarped <mybigb@gmail.com>
2018-04-11 11:25:19 +0200

    renamed schema-sql

--- bin/run.bat
+++ bin/run.bat
@@ -1,6 +1,6 @@
 python pgSqlExecutor.py ..\sql\drop.sql ..\config\postgis.json
 
-python pgSqlExecutor.py ..\sql\bigbadabom.sql ..\config\postgis.json
+python pgSqlExecutor.py ..\sql\schema.sql ..\config\postgis.json
 
 for %%f in (..\fme\*.fmw) do fme.exe %%f
 
