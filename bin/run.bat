8aa54b149dc92f6d6a848dfb8c18bf58e6085194
jarped <mybigb@gmail.com>
2018-04-11 12:20:50 +0200

    moved fme-import to separate folder

--- bin/run.bat
+++ bin/run.bat
@@ -2,7 +2,7 @@ python pgSqlExecutor.py ..\sql\drop.sql ..\config\postgis.json
 
 python pgSqlExecutor.py ..\sql\schema.sql ..\config\postgis.json
 
-for %%f in (..\fme\*.fmw) do fme.exe %%f
+for %%f in (..\fme\in\*.fmw) do fme.exe %%f
 
 python pgSqlExecutor.py ..\sql\post-process.sql ..\config\postgis.json
 
