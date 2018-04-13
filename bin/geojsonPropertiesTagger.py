9c898b94154e0974b9881f715588297d0f37b1d8
jarped <mybigb@gmail.com>
2018-04-13 11:29:01 +0200

    use empty string for tag-value and remove title where empty

--- bin/geojsonPropertiesTagger.py
+++ bin/geojsonPropertiesTagger.py
@@ -7,8 +7,10 @@ data = json.load(open(sys.argv[1]))
 
 for feature in data['features']:
     for code in feature['properties']['codes'].split(','):
-        feature['properties'][code] = "Null"
+        feature['properties'][code] = ""
     feature['properties'].pop('codes', None)
+    if feature['properties']['title'] == "": 
+        feature['properties'].pop('title', None)
 
 with open(sys.argv[2], 'w') as outfile:
     json.dump(data, outfile)
