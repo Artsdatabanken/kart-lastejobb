4c223e22ec150a6df19ed0978d624384c3cbb285
jarped <mybigb@gmail.com>
2018-04-16 16:05:53 +0200

    remove localid if empty

--- bin/geojsonPropertiesTagger.py
+++ bin/geojsonPropertiesTagger.py
@@ -3,14 +3,20 @@
 import sys
 import json
 
+properties = 'properties'
+removableProperties = [ 'localId', 'title', 'codes' ]
+
+
 data = json.load(open(sys.argv[1]))
 
 for feature in data['features']:
-    for code in feature['properties']['codes'].split(','):
-        feature['properties'][code] = ""
-    feature['properties'].pop('codes', None)
-    if feature['properties']['title'] == "": 
-        feature['properties'].pop('title', None)
+    for code in feature[properties]['codes'].split(','):
+        feature[properties][code] = ""
+    feature[properties]['codes'] = ""
+    for removableProperty in removableProperties:
+        if removableProperty in feature[properties]:
+            if feature[properties][removableProperty] == "": 
+                feature[properties].pop(removableProperty, None)
 
 with open(sys.argv[2], 'w') as outfile:
     json.dump(data, outfile)
