46b984c775207e41afbf13deb4e759a4b43ffca7
jarped <mybigb@gmail.com>
2018-05-09 13:37:39 +0200

    automate mbtile-creation

--- bin/export.bat
+++ bin/export.bat
@@ -1,5 +1,13 @@
 cd ..\fme\out
 
-for %%f in (*.fmw) do fme.exe %%f
+REM for %%f in (*.fmw) do fme.exe %%f
 
 cd ..\..\bin
+
+REM python .\geojsonPropertiesTagger.py D:\out\geojson\sqrt42\sqrt42.geojson D:\out\geojson\sqrt42\sqrt42tagged.geojson
+
+move D:\out\geojson\sqrt42\sqrt42.geojson D:\out\geojson\sqrt42\sqrt42.geojson.orig
+
+move D:\out\geojson\sqrt42\sqrt42tagged.geojson D:\out\geojson\sqrt42\sqrt42.geojson
+
+docker run -it --rm -v D:\out\geojson\sqrt42\:/data tippecanoe:latest tippecanoe -pk -P -zg -pS -o /data/sqrt42.mbtiles /data/sqrt42.geojson
