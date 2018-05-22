e26bd0961121512563f0869dc3f33ad3f2e551e6
jarped <mybigb@gmail.com>
2018-05-22 14:54:23 +0200

    run export from bin-folder

--- bin/export.bat
+++ bin/export.bat
@@ -6,13 +6,12 @@ cd ..\..\bin
 
 set out=D:\out\
 set geojson=%out%geojson\
-cd %geojson%
 
 for /D %%i in (%geojson%*) do (
-    python D:\git\grunnkart-dataflyt\bin\geojsonPropertiesTagger.py %%~ni\%%~ni.geojson %%~ni\%%~ni.geojson.tagged
-    move %%~ni\%%~ni.geojson %%~ni\%%~ni.geojson.orig 
-    move %%~ni\%%~ni.geojson.tagged %%~ni\%%~ni.geojson 
+    python .\geojsonPropertiesTagger.py %%i\%%~ni.geojson %%i\%%~ni.geojson.tagged
+    move %%i\%%~ni.geojson %%i\%%~ni.geojson.orig 
+    move %%i\%%~ni.geojson.tagged %%i\%%~ni.geojson 
     docker run -it --rm -v %%i:/data tippecanoe:latest tippecanoe -pk -P -zg -pS -o /data/%%~ni.mbtiles /data/%%~ni.geojson
     move %%i\%%~ni.mbtiles %out%mbtiles\
-    del %%~ni\%%~ni.geojson.orig
+    del %%i\%%~ni.geojson.orig
 )
