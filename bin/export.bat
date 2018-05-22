4158077a433c48a53bd38fdeac17fafb223ceee7
jarped <mybigb@gmail.com>
2018-05-22 15:03:23 +0200

    abort mbtile-generation for prefix on error

--- bin/export.bat
+++ bin/export.bat
@@ -1,17 +1,17 @@
-cd ..\fme\out
+REM cd ..\fme\out
 
-for %%f in (*.fmw) do fme.exe %%f
+REM for %%f in (*.fmw) do fme.exe %%f
 
-cd ..\..\bin
+REM cd ..\..\bin
 
 set out=D:\out\
 set geojson=%out%geojson\
 
 for /D %%i in (%geojson%*) do (
     python .\geojsonPropertiesTagger.py %%i\%%~ni.geojson %%i\%%~ni.geojson.tagged
-    move %%i\%%~ni.geojson %%i\%%~ni.geojson.orig 
-    move %%i\%%~ni.geojson.tagged %%i\%%~ni.geojson 
-    docker run -it --rm -v %%i:/data tippecanoe:latest tippecanoe -pk -P -zg -pS -o /data/%%~ni.mbtiles /data/%%~ni.geojson
-    move %%i\%%~ni.mbtiles %out%mbtiles\
-    del %%i\%%~ni.geojson.orig
+    if not errorlevel 1 move %%i\%%~ni.geojson %%i\%%~ni.geojson.orig
+    if not errorlevel 1 move %%i\%%~ni.geojson.tagged %%i\%%~ni.geojson
+    if not errorlevel 1 docker run -it --rm -v %%i:/data tippecanoe:latest tippecanoe -pk -P -zg -pS -o /data/%%~ni.mbtiles /data/%%~ni.geojson
+    if not errorlevel 1 del %%i\%%~ni.geojson.orig
+    if not errorlevel 1 move %%i\%%~ni.mbtiles %out%mbtiles\
 )
