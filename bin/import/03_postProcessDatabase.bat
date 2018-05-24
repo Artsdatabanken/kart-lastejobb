ff083be56f6d48ec4aca8ec3cd34bdfd434fee19
jarped <mybigb@gmail.com>
2018-05-24 10:22:38 +0200

    fixed relative paths

--- bin/import/03_postProcessDatabase.bat
+++ bin/import/03_postProcessDatabase.bat
@@ -1,3 +1,3 @@
-python ..\pgSqlExecutor.py ..\sql\post-process.sql ..\config\postgis.json
+python ..\pgSqlExecutor.py ..\..\sql\post-process.sql ..\..\config\postgis.json
 
-python ..\pgSqlExecutor.py ..\sql\vacuum.sql ..\config\postgis.json
+python ..\pgSqlExecutor.py ..\..\sql\vacuum.sql ..\..\config\postgis.json
