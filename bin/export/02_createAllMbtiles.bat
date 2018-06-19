cc63256acbc03e9b87cc3786c97bb933a2e354b3
jarped <mybigb@gmail.com>
2018-06-19 15:22:56 +0200

    keep original geojson

--- bin/export/02_createAllMbtiles.bat
+++ bin/export/02_createAllMbtiles.bat
@@ -3,9 +3,9 @@ set geojson=%out%geojson\
 
 for /D %%i in (%geojson%*) do (
     python ..\geojsonPropertiesTagger.py %%i\%%~ni.geojson %%i\%%~ni.geojson.tagged
-    if not errorlevel 1 move %%i\%%~ni.geojson %%i\%%~ni.geojson.orig
+    if not errorlevel 1 move %%i\%%~ni.geojson %%i\%%~ni.orig.geojson
     if not errorlevel 1 move %%i\%%~ni.geojson.tagged %%i\%%~ni.geojson
     if not errorlevel 1 docker run -it --rm -v %%i:/data tippecanoe:latest tippecanoe -pk -P -zg -pS -o /data/%%~ni.mbtiles /data/%%~ni.geojson
-    if not errorlevel 1 del %%i\%%~ni.geojson.orig
+    REM if not errorlevel 1 del %%i\%%~ni.orig.geojson
     if not errorlevel 1 move %%i\%%~ni.mbtiles %out%mbtiles\
 )
