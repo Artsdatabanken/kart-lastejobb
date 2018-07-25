223751175ea0ed10d9856aa348a8eac001eafabd
jarped <mybigb@gmail.com>
2018-07-25 10:49:54 +0200

    remove buffer from tiles in anticipation of Tangram

--- bin/export/02_createAllMbtiles.bat
+++ bin/export/02_createAllMbtiles.bat
@@ -5,7 +5,7 @@ for /D %%i in (%geojson%*) do (
     python ..\geojsonPropertiesTagger.py %%i\%%~ni.geojson %%i\%%~ni.geojson.tagged
     if not errorlevel 1 move %%i\%%~ni.geojson %%i\%%~ni.orig.geojson
     if not errorlevel 1 move %%i\%%~ni.geojson.tagged %%i\%%~ni.geojson
-    if not errorlevel 1 docker run -it --rm -v %%i:/data tippecanoe:latest tippecanoe -pk -P -zg -pS -o /data/%%~ni.mbtiles /data/%%~ni.geojson
+    if not errorlevel 1 docker run -it --rm -v %%i:/data tippecanoe:latest tippecanoe -b 0 -pk -P -zg -pS -o /data/%%~ni.mbtiles /data/%%~ni.geojson
     REM if not errorlevel 1 del %%i\%%~ni.orig.geojson
     if not errorlevel 1 move %%i\%%~ni.mbtiles %out%mbtiles\
 )
