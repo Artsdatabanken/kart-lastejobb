665bda61674b61e83d725b66077320e404fdf772
jarped <mybigb@gmail.com>
2018-08-07 14:28:46 +0200

    add reader-creation and grants to db-postprocessing

--- bin/import/03_postProcessDatabase.bat
+++ bin/import/03_postProcessDatabase.bat
@@ -1,3 +1,9 @@
 python ..\pgSqlExecutor.py ..\..\sql\post-process.sql ..\..\config\postgis.json
 
 python ..\pgSqlExecutor.py ..\..\sql\vacuum.sql ..\..\config\postgis.json
+
+python ..\pgSqlExecutor.py ..\..\docker\postgis\init\3_reader.sql ..\..\config\postgis.json
+
+python ..\pgSqlExecutor.py ..\..\docker\postgis\init\grant.txt ..\..\config\postgis.json
+
+python ..\pgSqlExecutor.py ..\..\sql\geometry_view.sql ..\..\config\postgis.json
--- docker/postgis/init/4_grant.sh
+++ docker/postgis/init/4_grant.sh
@@ -1,5 +1,1 @@
-echo 'GRANT USAGE ON SCHEMA data TO reader;
-GRANT USAGE ON SCHEMA public TO reader;
-GRANT SELECT ON ALL TABLES IN SCHEMA data TO reader;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO reader;
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO reader;' | psql bigbadabom
+cat /docker-entrypoint-initdb.d/grant.txt | psql bigbadabom
