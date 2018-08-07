cc41ff0f8051138b94eba947a4dca817e692595b
jarped <mybigb@gmail.com>
2018-08-07 10:51:37 +0200

    added text to view. added grant to reader

---
+++ sql/geometry_view.sql
@@ -0,0 +1,28 @@
+-- View: data.v_geometry
+
+-- DROP VIEW data.v_geometry;
+
+CREATE OR REPLACE VIEW data.v_geometry AS
+ SELECT g.id,
+    g.geography,
+    g.geometry,
+	c.code,
+	c.title,
+	CASE WHEN l_g.localid is null THEN ''
+     ELSE l_g.localid
+	END as localid,
+	p.value,
+	p.description as prefixdescription,
+	d.description as datasetdescription
+   FROM data.geometry g left join data.localid_geometry l_g on g.id =  l_g.geometry_id,
+    data.codes_geometry c_g,
+    data.dataset d,
+    data.prefix p,
+    data.codes c
+	where g.id = c_g.geometry_id and c_g.codes_id = c.id and g.dataset_id = d.id and p.id = d.prefix_id;
+
+ALTER TABLE data.v_geometry
+    OWNER TO postgres;
+	
+GRANT SELECT ON TABLE data.v_geometry TO reader;
+
