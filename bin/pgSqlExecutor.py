670e70ddffc0f8c12200d4778bbbd2b41fb22d0e
jarped <mybigb@gmail.com>
2018-08-23 14:21:23 +0200

    dirty version to allow fetching data

--- bin/pgSqlExecutor.py
+++ bin/pgSqlExecutor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import psycopg2
+import psycopg2.extras
 import json
 
 def createConnectionString():
@@ -18,4 +19,19 @@ def executeSql(sqlFile):
   conn.set_isolation_level(old_isolation_level)
   conn.close()
 
-executeSql(sys.argv[1])
+def writeResults(sqlFile, output):
+  conn = psycopg2.connect(createConnectionString())
+  old_isolation_level = conn.isolation_level
+  conn.set_isolation_level(0)
+  cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+  cursor.execute(open(sqlFile).read())
+  result = cursor.fetchall()
+  conn.commit()
+  cursor.close()
+  conn.set_isolation_level(old_isolation_level)
+  conn.close()
+  with open(output, 'w') as outfile:
+    json.dump(result, outfile)
+
+if len(sys.argv) == 3: executeSql(sys.argv[1])
+else: writeResults(sys.argv[1], sys.argv[3])
