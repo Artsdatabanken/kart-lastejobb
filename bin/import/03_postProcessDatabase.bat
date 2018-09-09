0c99d3972ef8a90ef29eaf3eec2ac572821ae896
jarped <mybigb@gmail.com>
2018-09-09 16:58:33 +0200

    fix reader

--- bin/import/03_postProcessDatabase.bat
+++ bin/import/03_postProcessDatabase.bat
@@ -2,8 +2,6 @@ python ..\pgSqlExecutor.py ..\..\sql\post-process.sql ..\..\config\postgis.json
 
 python ..\pgSqlExecutor.py ..\..\sql\vacuum.sql ..\..\config\postgis.json
 
-python ..\pgSqlExecutor.py ..\..\docker\postgis\init\3_reader.sql ..\..\config\postgis.json
-
-python ..\pgSqlExecutor.py ..\..\docker\postgis\init\grant.txt ..\..\config\postgis.json
+python ..\pgSqlExecutor.py ..\..\sql\reader.sql ..\..\config\postgis.json
 
 python ..\pgSqlExecutor.py ..\..\sql\geometry_view.sql ..\..\config\postgis.json
