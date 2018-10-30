b68109b49f02c9ec6770df6435ced501a00b7661
jarped <mybigb@gmail.com>
2018-10-30 09:57:01 +0100

    run RL-classification

--- bin/import/03_postProcessDatabase.bat
+++ bin/import/03_postProcessDatabase.bat
@@ -5,3 +5,5 @@ python ..\pgSqlExecutor.py ..\..\sql\vacuum.sql ..\..\config\postgis.json
 python ..\pgSqlExecutor.py ..\..\sql\reader.sql ..\..\config\postgis.json
 
 python ..\pgSqlExecutor.py ..\..\sql\geometry_view.sql ..\..\config\postgis.json
+
+D:\install\redList\publish\Rodliste.exe ..\..\config\postgis.json
