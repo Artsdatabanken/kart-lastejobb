894156f7904f993fad39e15b13b12f2ea5277996
jarped <mybigb@gmail.com>
2018-11-08 11:22:26 +0100

    moved mbtiles output to polygon-folder

--- bin/export/02_createAllMbtiles.bat
+++ bin/export/02_createAllMbtiles.bat
@@ -4,10 +4,11 @@ set geojson=%out%geojson\codes\
 
 for %%i in (%geojson%*) do (
     echo Creating mbtile for %%~ni.geojson
-    docker run -it --rm -v %geojson%:/data tippecanoe:latest tippecanoe -b 0 -zg -ab --simplification=10 --increase-gamma-as-needed -pS -S 10 -l %%~ni -o /data/%%~ni.mbtiles /data/%%~ni.geojson
-    REM More aggressive simplification wit -r and -g
+    docker run -it --rm -v %geojson%:/data tippecanoe:latest tippecanoe -b 0 -zg -ab --simplification=10 --drop-densest-as-needed --increase-gamma-as-needed -pS -S 10 -l %%~ni -o /data/%%~ni.mbtiles /data/%%~ni.geojson
+    REM More aggressive simplification with -r and -g
     REM docker run -it --rm -v %geojson%:/data tippecanoe:latest tippecanoe -b 0 -zg -ab -r 5 -g 3 --simplification=10 --increase-gamma-as-needed -pS -S 10 -l %%~ni -o /data/%%~ni.mbtiles /data/%%~ni.geojson
     REM Only drop densest
     REM docker run -it --rm -v %geojson%:/data tippecanoe:latest tippecanoe -b 0 -zg -ab --drop-densest-as-needed -pS -S 10 -l %%~ni -o /data/%%~ni.mbtiles /data/%%~ni.geojson
 )
-move %geojson%*.mbtiles %out%\mbtiles\codes\
+mkdir %out%\mbtiles\polygon\
+move %geojson%*.mbtiles %out%\mbtiles\polygon\
