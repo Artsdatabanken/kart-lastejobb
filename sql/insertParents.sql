d3ac0a5665e407e2f0c0080012493dd3f6462699
jarped <mybigb@gmail.com>
2018-12-03 15:45:43 +0100

    removes duplicates

--- sql/insertParents.sql
+++ sql/insertParents.sql
@@ -1,14 +1,15 @@
 insert into data.codes_geometry (codes_id, geometry_id, code, fraction, created)
-select p.id as codes_id, cg.geometry_id, p.code, cg.fraction, cg.created from 
+select p.id as codes_id, cg.geometry_id, p.code, max(cg.fraction), max(cg.created)
+from 
 data.codes s,
 data.codes p,
 data.codes_geometry cg
 where s.id = cg.codes_id
-and p.predecessor = s.code
+and p.code = s.predecessor
 and not exists(
 select 1 from 
 data.codes_geometry cg1
 where cg1.codes_id = p.id
 and  cg1.geometry_id = cg.geometry_id
-
 )
+group by p.id , cg.geometry_id, p.code 
