b24d67557d869ed4d81514ad9e9bcdece87c482e
jarped <mybigb@gmail.com>
2018-12-19 12:23:57 +0100

    rearranged order of postprocessing

--- bin/import/03_postProcessDatabase.bat
+++ bin/import/03_postProcessDatabase.bat
@@ -1,13 +1,16 @@
-python ..\pgSqlExecutor.py ..\..\sql\post-process.sql ..\..\config\postgis.json
+python .\codes.py ..\..\config\postgis.json
 
-python ..\pgSqlExecutor.py ..\..\sql\vacuum.sql ..\..\config\postgis.json
+python ..\pgSqlExecutor.py ..\..\sql\post-process.sql ..\..\config\postgis.json
 
 python ..\pgSqlExecutor.py ..\..\sql\reader.sql ..\..\config\postgis.json
 
-python ..\pgSqlExecutor.py ..\..\sql\geometry_view.sql ..\..\config\postgis.json
-
-D:\install\redList\Redlist.exe ..\..\config\postgis.json
-
-python .\codes.py ..\..\config\postgis.json
+python ..\pgSqlExecutor.py ..\..\sql\vacuum.sql ..\..\config\postgis.json
 
 "C:\Program Files\PostgreSQL\10\bin\pg_dump.exe" -U postgres --no-password --format=c --file=d:\out\db\bigbadabom.backup bigbadabom
+
+python ..\pgSqlExecutor.py ..\..\sql\geometry_view.sql ..\..\config\postgis.json
+
+python ..\pgSqlExecutor.py ..\..\sql\vacuum.sql ..\..\config\postgis.json
+
+REM D:\install\redList\Redlist.exe ..\..\config\postgis.json
+
