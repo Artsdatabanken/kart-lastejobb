eb80acc83ff0355b9f627d2d5ee5d65756a667f0
jarped <mybigb@gmail.com>
2018-12-19 16:12:49 +0100

    cluster and refresh materialized view

--- sql/geometry_view.sql
+++ sql/geometry_view.sql
@@ -1,3 +1,7 @@
 REFRESH MATERIALIZED VIEW data.v_geometry;
 
 CLUSTER data.v_geometry USING v_geometry_geometry_idx;
+
+REFRESH MATERIALIZED VIEW data.v_geometry_all;
+
+CLUSTER data.v_geometry_all USING v_geometry_allgeometry_idx;
